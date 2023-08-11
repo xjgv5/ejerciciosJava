53fdfb58a45e9f22fbb473a4e0303ae81aa583ef
Javier Gomez <javier.gov@outlook.es>
2023-08-11 00:31:09 -0600

    Ejemplo de paso por valor

---
+++ src/PasarPorValor.java
@@ -0,0 +1,15 @@
+public class PasarPorValor {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("iniciamos el main = " + i);
+        test(i);
+        System.out.println("Finaliza main con i = " + i);
+        //pasa solamente el valor, no la referencia 
+    }
+
+    public static void test(int i ){
+        System.out.println("Iniciamos el metodo test con i  = "+ i);
+         i = 35;
+        System.out.println("Finaliza el valor de i  = " + i);
+    }
+}
