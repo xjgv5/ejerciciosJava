b7af3e5bd126d3f05d9c306a7e3ffc8b101f4d8a
Javier Gomez <javier.gov@outlook.es>
2023-08-11 11:47:54 -0600

    Ejemplo de uso de Java Date

---
+++ src/EjemploJavaDate.java
@@ -0,0 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class EjemploJavaDate {
+    public static void main(String[] args) {
+        Date fecha = new Date();
+
+        System.out.println("fecha = " + fecha);
+
+        SimpleDateFormat df = new SimpleDateFormat("EEEE dd MMMM yyyy 'a las' HH:mm" );
+        String fechaStr = df.format(fecha);
+        System.out.println(fechaStr);
+    }
+}
