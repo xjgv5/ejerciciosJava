5ac4e2a86b0176670dfbe1d8e3eb0839a575bf49
Javier Gomez <javier.gov@outlook.es>
2023-08-14 22:56:26 -0600

    Uso de clase MATH

--- src/EjemploClaseMath.java
+++ src/EjemploClaseMath.java
@@ -20,5 +20,20 @@ public class EjemploClaseMath {
 
         long redondeo = Math.round(Math.PI);
         System.out.println("redondeo = " + redondeo);
+
+        double exp = Math.exp(2);
+        System.out.println("exp = " + exp);
+
+        double log = Math.log(10);
+        System.out.println("log = " + log);
+
+        double potencia = Math.pow(2, 3);
+        System.out.println("potencia = " + potencia);
+
+        double raiz = Math.sqrt(9);
+        System.out.println("raiz = " + raiz);
+
+        double grados = Math.round(Math.toDegrees(1.57));
+        System.out.println("Convertir radianes a grados = " + grados);
     }
 }
