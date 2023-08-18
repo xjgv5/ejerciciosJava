2141a2797e50b9efd4de91c1836162444ac7568b
Javier Gomez <javier.gov@outlook.es>
2023-08-18 00:28:06 -0600

    ordenando pares e impares de un arreglo

---
+++ src/ArregloParesImpares.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class ArregloParesImpares {
+    public static void main(String[] args) {
+        int[] a, pares, impares;
+        a = new int[10];
+        int totalPares = 0;
+        int totalImpares = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingresa 10 numeros : ");
+
+        for (int i = 0; i <a.length; i++){
+            a[i] = s.nextInt();
+        }
+
+        for (int i = 0; i< a.length; i++){
+            if (a[i] % 2 == 0){
+                totalPares += 1;
+            } else {
+                totalImpares += 1;
+            }
+        }
+
+        pares = new int[totalPares];
+        impares = new int[totalImpares];
+
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < a.length; i++){
+            if(a[i] % 2 == 0){
+                pares[j++] = a[i];
+            } else
+                impares[k++] = a[i];
+        }
+
+        System.out.println("Numeros pares : ");
+        for (int i = 0; i < pares.length; i++){
+            System.out.println(pares[i]);
+        }
+        System.out.println("--------------------");
+        System.out.println("Numeros impares");
+        for (int i = 0; i < impares.length; i++){
+            System.out.println(impares[i]);
+        }
+
+    }
+}
