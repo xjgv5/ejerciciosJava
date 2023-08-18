a16d04aa2f401e40624b117e7c7d40045a4fffc8
Javier Gomez <javier.gov@outlook.es>
2023-08-17 23:39:25 -0600

    Combinando 2 arreglos en 1

---
+++ src/CombinarArreglos.java
@@ -0,0 +1,27 @@
+public class CombinarArreglos {
+    public static void main(String[] args) {
+        int[] a,b,c;
+        a = new int[10];
+        b = new int[10];
+        c = new int[20];
+
+        for (int i = 0; i<a.length; i++){
+            a[i] = i + 1;
+        }
+
+        for (int i = 0; i<b.length; i++){
+            b[i] = (i + 1) * 5;
+        }
+
+        int aux = 0;
+        for (int i = 0; i< 10; i++){
+            c[aux++] = a[i];
+            c[aux++] = b[i];
+        }
+
+        aux = 0;
+        for (int i = 0; i < c.length; i++){
+            System.out.print(c[i]);
+        }
+    }
+}
