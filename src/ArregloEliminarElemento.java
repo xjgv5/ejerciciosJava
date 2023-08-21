074422b386996d6f89e497cf7ebbfbd4a6efc34d
Javier Gomez <javier.gov@outlook.es>
2023-08-20 23:04:32 -0600

    Eliminar un elemento de un arreglo

--- src/ArregloEliminarElemento.java
+++ src/ArregloEliminarElemento.java
@@ -22,6 +22,15 @@ public class ArregloEliminarElemento {
         for (int i = 0;  i < tamanio; i++){
             System.out.println( i +" =>" + a[i]);
         }
+        System.out.println("-----------------------------------");
+        // ----------------------------------------------
+        //copiando en arreglo
+        int[] b = new int[a.length - 1];
+        System.arraycopy(a, 0, b,0 , b.length);
+        System.out.println("Arreglo b : ");
+        for (int i = 0; i < b.length; i++){
+            System.out.println(i + " => " + b[i]);
+        }
 
     }
 }
