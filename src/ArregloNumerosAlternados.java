8df609e3745393e0347c6ef5cc0e92412115a501
Javier Gomez <javier.gov@outlook.es>
2023-08-22 23:17:52 -0600

    Mostrar datos alternados de un arreglo

--- src/ArregloNumerosAlternados.java
+++ src/ArregloNumerosAlternados.java
@@ -6,10 +6,12 @@ public class ArregloNumerosAlternados {
         int[] a = new int[10];
         Scanner consola = new Scanner(System.in);
 
+        System.out.println("Programa que muestra los numeros alternados ! ");
         //llenando el arreglo :
         for (int i = 0; i < a.length; i++){
-            System.out.println("Ingresa un numero ");
-            a[i] = consola.nextInt();
+           // System.out.println("Ingresa un numero ");
+            //a[i] = consola.nextInt();
+            a[i] = i + 1;
         }
 
         System.out.println("------------------------");
@@ -18,9 +20,29 @@ public class ArregloNumerosAlternados {
         System.out.println();
         System.out.println("------------------------");
         System.out.println("Arreglo modificado");
-        for (int i = 0; i<a.length; i++){
-            
-        }
+        int aux = a.length - 1;
+        int i = 0;
+        int contador = 0;
+        do{
+            if (contador % 2 != 0){
+                System.out.println(a[i]);
+                i++;
+            } else if (contador %2 == 0){
+                System.out.println(a[aux]);
+                aux--;
+            }
+
+            contador++;
+
+        }while (contador < a.length);
+
+        // como en cada iteración del for se muestran dos números
+        // solo necesitaremos recorrer la mitad de iteraciones, con eso es suficiente.
+        /*System.out.println("El resultado es:");
+        for (i = 0; i <= 4; i++) {
+            System.out.println(arreglo[9 - i]); // y el i-ésimo por el final
+            System.out.println(arreglo[i]); // mostramos el i-ésimo número por el principio
+        }*/
 
     }
 
